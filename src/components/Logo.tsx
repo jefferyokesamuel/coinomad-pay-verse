311db06ce182a6498450a636dbde580051a894ae
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2025-06-06 14:17:53 +0100

    UI Fixes

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import logo from '../assets/logo.png'
 const Logo: React.FC = () => {
-  return <div className="flex items-center">
-      <img src={logo} alt="Coinomad Logo" className="h-[130px]" />
+  return <div className="flex items-center h-16 overflow-hidden">
+      <img src={logo} alt="Coinomad Logo" className="h-[165px] w-[165px] object-contain" />
     </div>;
 };
 export default Logo;
